8966b3723138f750e7cfaf237c06e9624738bb18
billhartcp <bill.hart@crownpeak.com>
2019-03-13 11:39:39 +0000

    Amended push script

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Script for loggin into Dockerhub and pushing image to repo
 export VERSION=`cat version.txt`
 echo "$DOCKER_PW" | docker login -u "$DOCKER_LOGIN" --password-stdin
 docker tag local/apachetest:$VERSION crownpeak/apachetest:$VERSION
